6342cdf441bab7aafdb610d945bbea2a74cb1549
YogeshrajaaR <124078907+YogeshrajaaR@users.noreply.github.com>
2023-02-01 16:18:02 +0530

    Added ex11

---
+++ ex11.rb
@@ -0,0 +1,7 @@
+print "What's is your name? "
+name = gets.chomp
+print "What do you want to be become? "
+aim = gets.chomp
+print "what is your favorite colour? "
+colour = gets.chomp
+puts "My name is #{name} \nMy favorite colour is #{colour}. \nI will become a #{aim} in future. "
